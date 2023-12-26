8018f9a9a250235cb589b8df728e8c84d9131acd
Nikita7696 <Nickya0796@gmail.com>
2023-12-27 00:31:36 +0530

    Assignment done for Variable
    Assignment done for Variable

---
+++ 01_variableAssign.js
@@ -0,0 +1,49 @@
+
+
+
+var myFullName
+console.log(myFullName);
+
+var myFullName= "Nikita Ghorpade";//
+console.log(myFullName);
+console.log("my first & last Name is",myFullName);
+
+
+myFullName=" Nikita Prasad Ghorpade"
+console.log("My Full Name is", myFullName);
+
+
+
+var myLove= "I love only Js";//
+console.log (myLove)
+
+var firstName="Nikita";
+var lastName="Ghorpade";
+var age="27";
+var address="Pune";
+var pincode=" 410 504";
+var gender=" Female";
+var ismarried="Married";
+
+
+console.log(" Fisrt name is", firstName)
+console.log(" Last name is", lastName)
+console.log(" Age->", age)
+console.log(" Address->", address)
+console.log(" Pincode is", pincode)
+console.log(" Gender is", gender)
+console.log(" Yes I am", ismarried)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
